cabaa77ee15cc3c68263de79931cf4b4d78a5f7a
Shorebyte Labs <shorebytelabs@gmail.com>
2025-11-06 16:28:21 -0800

    Rename App.tsx → App.js and replace template with custom title

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Nails By Abri</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+});
+
+export default App;
